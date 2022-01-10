552fa765ec0dea7f5643eda3a669d32ca1667203
tgsheng <15626185060@163.com>
2022-01-10 15:07:05 +0800

    ex7_06.h

--- Cpp-Primer-5e/ch07/ex7_06.h
+++ Cpp-Primer-5e/ch07/ex7_06.h
@@ -1,44 +1,40 @@
-#ifndef CP5_ex7_06_h
-#define CP5_ex7_06_h
+#ifndef CP5_ex7_06_H
+#define CP5_ex7_06_H
 
-#include <string>
 #include <iostream>
+#include <string>
 
 struct Sales_data {
-    std::string const& isbn() const { return bookNo; };
-    Sales_data& combine(const Sales_data&);
+  std::string const &isbn() const { return bookNo; };
+  Sales_data &combine(const Sales_data &);
 
-    std::string bookNo;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
+  std::string bookNo;
+  unsigned units_sold = 0;
+  double revenue = 0.0;
 };
 
-inline Sales_data& Sales_data::combine(const Sales_data& rhs)
-{
-    units_sold += rhs.units_sold;
-    revenue += rhs.revenue;
-    return *this;
+inline Sales_data &Sales_data::combine(const Sales_data &rhs) {
+  units_sold += rhs.units_sold;
+  revenue += rhs.revenue;
+  return *this;
 }
 
-inline std::istream &read(std::istream &is, Sales_data &item)
-{
-    double price = 0;
-    is >> item.bookNo >> item.units_sold >> price;
-    item.revenue = price * item.units_sold;
-    return is;
+inline std::istream &read(std::istream &is, Sales_data &item) {
+  double price = 0;
+  is >> item.bookNo >> item.units_sold >> price;
+  item.revenue = price * item.units_sold;
+  return is;
 }
 
-inline std::ostream &print(std::ostream &os, const Sales_data &item)
-{
-    os << item.isbn() << " " << item.units_sold << " " << item.revenue;
-    return os;
+inline std::ostream &print(std::ostream &os, const Sales_data &item) {
+  os << item.isbn() << " " << item.units_sold << " " << item.revenue;
+  return os;
 }
 
-inline Sales_data add(const Sales_data &lhs, const Sales_data &rhs)
-{
-    Sales_data sum = lhs;
-    sum.combine(rhs);
-    return sum;
+inline Sales_data add(const Sales_data &lhs, const Sales_data &rhs) {
+  Sales_data sum = lhs;
+  sum.combine(rhs);
+  return sum;
 }
 
-#endif
+#endif /* CP5_ex7_05_H */
