806c5606f9468ac2b7fc1591be71db9776bfe8e6
tgsheng <15626185060@163.com>
2022-01-23 04:41:38 +0800

    ex12_05

--- Cpp-Primer-5e/ch12/ex12_05.cpp
+++ Cpp-Primer-5e/ch12/ex12_05.cpp
@@ -3,11 +3,17 @@
 
 void test(const StrBlob &strBlob) { return; }
 
+/*
+When a constructor is declared explicit, it can be used only with the
+direct form of initialization (§ 3.2.1, p. 84). Moroever, the compiler will
+not use this constructor in an automatic conversion
+*/
+
 int main() {
   // StrBlob strBlob = {"aa", "bb", "cc"}; // Explicit constructor
   StrBlob strBlob({"aa", "bb", "cc"});
 
   // test({"aa", "bb", "cc"}); // cannot convert initializer list argument to
-                               // 'const StrBlob'
+  // 'const StrBlob'
   test(StrBlob({"aa", "bb", "cc"}));
 }
